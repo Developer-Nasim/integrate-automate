06a442d3ce261746ce67ea536df1de329e997dd0
Developer-Nasim <unique0coder@gmail.com>
2021-02-02 12:33:04 +0600

    full-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,20 @@
 
 
 
+let loader = document.querySelector('.prealoder-area')
+window.addEventListener('load', () => {
+    loader.classList.add('none');
+})
+
+
+
+
+
+
+// menu 
+$('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
  AOS.init();
