130da218beecc79e3f527b540b3c934609f0b211
Developer-Nasim <unique0coder@gmail.com>
2021-02-18 03:30:08 +0600

    pages up

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,30 @@ $('.siteBar-btn').click( function (){
 
 
 let loader = document.querySelector('.prealoder-area')
+let cbtn   = document.querySelectorAll('.contact-btn')
+let ctSec  = document.querySelector('.contact-area')
 window.addEventListener('load', () => {
     loader.classList.add('none');
     setTimeout(() => {
         AOS.init();
     }, 1000);
+
+    contactSection();
 })
  
+function contactSection(e){
+    console.log(e)
+    cbtn.forEach(cgct => {
+        cgct.addEventListener('click', () => {
+            let a = 0;
+            setInterval(() => {
+                if (a >= ctSec.offsetTop) {
+                    a = ctSec.offsetTop
+                }else{ 
+                    document.documentElement.scrollTop = a;
+                    a = 10 + a++;
+                }
+            }, 1);
+        })
+    })
+}
